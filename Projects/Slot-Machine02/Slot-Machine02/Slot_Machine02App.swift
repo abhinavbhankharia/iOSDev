f865b1cef4c8bf4ac1cf48771be1e06f1463df12
abhinavbhankharia <abhinavbhankharia@gmail.com>
2025-06-01 23:57:43 +0530

    Building Slot Machine Game and learning how to buttons work

---
+++ Projects/Slot-Machine02/Slot-Machine02/Slot_Machine02App.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_Machine02App.swift
+//  Slot-Machine02
+//
+//  Created by Abhinav Bhankharia on 31/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Slot_Machine02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
