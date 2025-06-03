f6ebb3e378a2e98c3e2e1423f3802f8201c8380a
abhinavbhankharia <abhinavbhankharia@gmail.com>
2025-06-03 22:12:16 +0530

    Getting started with my third iOS project - Calculator App

---
+++ Projects/Calculator03/Calculator03/Calculator03App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator03App.swift
+//  Calculator03
+//
+//  Created by Abhinav Bhankharia on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Calculator03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
