047a700d30878fab96d5806b971312cd1e637352
abhinavbhankharia <abhinavbhankharia@gmail.com>
2025-06-05 23:48:12 +0530

    Getting started with shopping app and importing assets

---
+++ Projects/shoppingApp05/shoppingApp05/shoppingApp05App.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingApp05App.swift
+//  shoppingApp05
+//
+//  Created by Abhinav Bhankharia on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct shoppingApp05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
