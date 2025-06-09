b42a4c9d115c66e69bcb74519185bd8216751a7b
abhinavbhankharia <abhinavbhankharia@gmail.com>
2025-06-09 21:23:49 +0530

    Building a model for items in ToDo list as identifiable and codable

--- Projects/TodoApp07/TodoApp07/Model/ItemModel.swift
+++ Projects/TodoApp07/TodoApp07/Model/ItemModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct ItemModel: Identifiable, Codable {
+    let title: String
+    let isCompleted: Bool
+    let id: String
+    
+    //to follow ide
+    init( Id: String = UUID().uuidString, Title: String, IsCompleted: Bool){   //to follow identifiable protocol, we will be generating a unique ID which is a string
+    //initializer will create a unique ID whenever struct is used
+        self.id = Id
+        self.title = Title
+        self.isCompleted = IsCompleted
+    }
+    
+    //updation method
+    
+    func updateCompleted() -> ItemModel {
+        
+        return ItemModel(Id: id, Title: title, IsCompleted: !isCompleted)
+    }
+    
+}
